707a54ece64e069a2e015e29e4c02bcb3396e731
Steven Khong <khongwh@gmail.com>
2024-11-05 11:54:44 +0800

    use extension to construct query client

--- packages/core/src/client/client.ts
+++ packages/core/src/client/client.ts
@@ -1,10 +1,5 @@
-import { Tendermint37Client } from "@cosmjs/tendermint-rpc";
-import { createProtobufRpcClient, QueryClient } from "@cosmjs/stargate";
-import { QueryClientImpl } from "@demex-sdk/codecs/src/Switcheo/carbon/coin/query";
 
 export class DemexClient {
-  constructor(tmClient: Tendermint37Client) {
-    const rpcClient = createProtobufRpcClient(new QueryClient(tmClient));
-    new QueryClientImpl(rpcClient);
+  constructor() {
   }
 }
