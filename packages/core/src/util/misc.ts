96f036b0b1e043fc305900ed784298bd16e00f3c
Steven Khong <khongwh@gmail.com>
2024-12-05 17:36:08 +0800

    remove unused types

--- packages/core/src/util/misc.ts
+++ packages/core/src/util/misc.ts
@@ -6,7 +6,3 @@ export const callIgnoreError = <T = any>(runnable: () => T) => {
 
 export type MaybePromise<T> = T | PromiseLike<T>
 export type UnwrapPromise<T> = T extends PromiseLike<infer V> ? V : T
-export type CastAny<T, CastTo> = IsAny<T, CastTo, T>
-export type IsAny<T, True, False = never> = true | false extends (
-  T extends never ? true : false
-) ? True : False
